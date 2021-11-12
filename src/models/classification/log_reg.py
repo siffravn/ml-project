1e6b9d1fe8d6974f9a4c667b292c613818be4cbf
Siff Ravn <siff.ravn@gmail.com>
2021-11-12 15:47:47 +0100

    P2: feat(classification) - implement log_reg util class

---
+++ src/models/classification/log_reg.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+def define_train_test(lambda_val, X_train, y_train, X_test, y_test):
+    
+    # Calculate mean and std for the training set
+    mu = np.mean(X_train, 0)
+    sigma = np.std(X_train, 0)
+    
+    # Define, train and test model
+    model = define(lambda_val)
+    model = train(model, X_train, y_train, mu, sigma)
+    error_rate = test(model, X_test, y_test, mu, sigma)
+    
+    return error_rate
+
+
+def define(lambda_val):
+    
+    model = LogisticRegression(penalty='l2', C=1/lambda_val)
+    
+    return model
+
+def train(model, X_train, y_train, mu, sigma):
+    
+    # Standardize the training based on its mean and std
+    mu = np.mean(X_train, 0)
+    sigma = np.std(X_train, 0)
+    
+    X_train = (X_train - mu) / sigma
+    
+    model.fit(X_train, y_train)
+    
+    return model
+
+def test(model, X_test, y_test, mu, sigma):
+    
+    # Standardize the test set based on training set mean and std
+    X_test = (X_test - mu) / sigma
+    
+    y_test_est = model.predict(X_test).T 
+    error_rate = np.sum(y_test_est != y_test) / len(y_test)
+    
+    return error_rate
